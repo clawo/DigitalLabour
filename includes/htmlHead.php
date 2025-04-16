988d5fd43728c27991773daadfafe84341f3c967
felixvoss <felix.voss@myhsba.de>
2025-04-16 12:27:12 +0200

    test css

--- includes/htmlHead.php
+++ includes/htmlHead.php
@@ -1,37 +1,32 @@
 <?php
-// ============================
-//   Initialisierung & DB-Connect
-// ============================
-
 session_start();
-
 require_once 'db_connect.php';
 $db = getDB();
 
-// Aktuellen Dateinamen ohne ".php"-Endung holen
+// Aktuelle Seite
 $currentPage = basename($_SERVER['PHP_SELF'], '.php');
 
-// Absoluter Pfad zur CSS-Datei
+// Absoluter Pfad auf dem Server (für file_exists)
 $cssPath = __DIR__ . '/../css/style' . ucfirst($currentPage) . '.css';
 
-// Relativer Link zur CSS-Datei (für HTML <link>)
-$cssLink = 'css/style' . ucfirst($currentPage) . '.css';
+// Öffentlicher URL-Link (fix: mit /digitallabour/)
+$cssLink = '/digitallabour/css/style' . ucfirst($currentPage) . '.css';
 ?>
 
-<!-- ========== Basis-HTML-Kopf ========== -->
+<!-- Meta & Base -->
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<base href="/digitallabour/"> <!-- Stellt sicher, dass relative Pfade stimmen -->
+<base href="/digitallabour/">
 
-<!-- ========== Fonts ========== -->
+<!-- Fonts -->
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
 
-<!-- ========== Globales CSS ========== -->
-<link rel="stylesheet" href="css/styleGlobal.css">
+<!-- Global Styles -->
+<link rel="stylesheet" href="/digitallabour/css/styleGlobal.css">
 
-<!-- ========== Automatisch korrektes CSS einbinden ========== -->
+<!-- Dynamisches Seiten-CSS -->
 <?php if (file_exists($cssPath)): ?>
   <link rel="stylesheet" href="<?= $cssLink ?>">
 <?php endif; ?>
