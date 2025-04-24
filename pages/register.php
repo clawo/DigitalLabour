cce84e3bd6c7a58e793b4778ed181bf38af77200
DankoRother <dankorother1@gmail.com>
2025-04-24 14:31:59 +0200

    Update register.php

--- pages/register.php
+++ pages/register.php
@@ -23,7 +23,7 @@
       Schon einen Account? <a href="login.php">Jetzt anmelden!</a>
     </p>
 
-    <button type="submit" class="register-btn">Registrieren</button>
+    <button onclick="handleRegister()" type="submit" class="register-btn">Registrieren</button>
   </form>
 </main>
 
