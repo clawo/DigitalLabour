f2c7862dfa077fe141a0b730c8a3507d089aedb4
Ben <68445452+myBennnn@users.noreply.github.com>
2025-04-24 15:54:43 +0200

    Revert register.php

--- pages/register.php
+++ pages/register.php
@@ -2,6 +2,7 @@
 session_start();
 
 require_once __DIR__ . '/../includes/htmlHead.php';
+require_once __DIR__ . '/../includes/header.php';
 require_once __DIR__ . '/../db_connect.php';
 require_once __DIR__ . '/../db_controller.php';
 
@@ -14,10 +15,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {             // nur verarbeiten,
     $errorMessage = $database_controller->handleRegister($database_controller);
 }
 ?>
-<body>
-<header>
-    <?php require_once __DIR__ . '/../includes/header.php'; ?>
-</header>
 <main class="register-container">
     <h1 class="register-title">REGISTRIEREN</h1>
 
@@ -47,6 +44,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {             // nur verarbeiten,
         <button type="submit" class="register-btn">Registrieren</button>
     </form>
 </main>
-</body>
 
 <?php include __DIR__ . '/../includes/footer.php'; ?>
