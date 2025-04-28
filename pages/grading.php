ec5bd2c3b2e23135f125edc6540c7cb72eb31ba0
clawo <131432761+clawo@users.noreply.github.com>
2025-04-28 12:50:20 +0200

    page Grading added

---
+++ pages/grading.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <?php include '../includes/htmlHead.php'; ?>
+    <title>Grading with ChatGPT</title>
+</head>
+
+<?php include '../includes/header.php'; ?>
+
+<body>
+
+<div class="contentBody">
+<h1>Aufgaben: "Modul 1"</h1>
+
+    <div class="section">
+        <strong><h2>Teil A:</h2></strong> <h2>Wissensfragen</h2>
+    </div>
+
+    <div class="section question">
+        <h3><strong>Beispielfrage:</strong></h3>
+        <p>1. Was versteht man unter dem Begriff "Nachhaltigkeit"? </p>
+        <label for="antwort">Antwort:</label><br>
+        <textarea> Placeholder </textarea>
+    </div>
+
+    <div class="section question">
+        <h3><strong>Begründung der Note durch ChatGPT</strong></h3>
+        <label for="antwort">Antwort:</label><br>
+        <textarea> Placeholder </textarea>
+    </div>
+
+    <div class="section question">
+        <h3><strong>Note</strong></h3>
+        <textareaGrade> Placeholder </textareaGrade>
+    </div>
+
+    <div class="button-container">
+        <button>Weiter</button>
+    </div>
+</div>
+</body>
+
+<?php include '../includes/footer.php'; ?>
+
+</html>
