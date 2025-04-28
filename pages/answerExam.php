cd39eae6cc2c5f805087bc513ae755bc2a4cc841
clawo <131432761+clawo@users.noreply.github.com>
2025-04-28 12:31:11 +0200

    changes

--- pages/answerExam.php
+++ pages/answerExam.php
@@ -9,21 +9,24 @@
 <?php include '../includes/header.php'; ?>
 
 <body>
+
+<div class="contentBody">
 <h1>Aufgaben: "Modul 1"</h1>
 
-<div class="section">
-    <strong>Teil A:</strong> Wissensfragen (z. B. Multiple Choice, Kurzantworten)
-</div>
+    <div class="section">
+        <strong>Teil A:</strong> Wissensfragen 
+    </div>
 
-<div class="section question">
-    <p><strong>Beispielfrage:</strong></p>
-    <p>1. Was versteht man unter dem Begriff "Nachhaltigkeit"? (2 Punkte)</p>
-    <label for="antwort">Antwort:</label><br>
-    <textarea id="antwort" name="antwort"></textarea>
-</div>
+    <div class="section question">
+        <p><strong>Beispielfrage:</strong></p>
+        <p>1. Was versteht man unter dem Begriff "Nachhaltigkeit"? </p>
+        <label for="antwort">Antwort:</label><br>
+        <textarea id="antwort" name="antwort"></textarea>
+    </div>
 
-<div class="button-container">
-    <button>Weiter</button>
+    <div class="button-container">
+        <button>Weiter</button>
+    </div>
 </div>
 </body>
 
