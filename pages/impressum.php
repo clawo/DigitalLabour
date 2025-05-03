1109b154a04f3f8469250c36df15901b88c48ee4
clawo <131432761+clawo@users.noreply.github.com>
2025-05-03 22:50:12 +0200

    impressum added

--- pages/impressum.php
+++ pages/impressum.php
@@ -3,13 +3,59 @@
 
 <head>
     <?php include '../includes/htmlHead.php'; ?>
-    <title>Titel</title>
+    <title>Impressum</title>
 </head>
 
 <?php include '../includes/header.php'; ?>
 
 <body>
-    <p>Text</p>
+<div class="container">
+    <h1>Impressum</h1>
+
+    <h2>Angaben gemäß § 5 TMG</h2>
+    <p>
+      HSBA – Hamburg School of Business Administration<br />
+      Alter Wall 38<br />
+      20457 Hamburg<br />
+      Deutschland
+    </p>
+
+    <h2>Vertreten durch:</h2>
+    <p>
+      Prof. Dr. Inga Schmidt (Präsidentin)
+    </p>
+
+    <h2>Kontakt</h2>
+    <p>
+      Telefon: +49 (0)40 36138-700<br />
+      Fax: +49 (0)40 36138-751<br />
+      E-Mail: <a href="mailto:info@hsba.de">info@hsba.de</a><br />
+      Website: <a href="https://www.hsba.de" target="_blank">www.hsba.de</a>
+    </p>
+
+    <h2>Umsatzsteuer-ID</h2>
+    <p>
+      Umsatzsteuer-Identifikationsnummer gemäß § 27 a Umsatzsteuergesetz:<br />
+      DE 813 496 679
+    </p>
+
+    <h2>Aufsichtsbehörde</h2>
+    <p>
+      Behörde für Wissenschaft, Forschung, Gleichstellung und Bezirke der Freien und Hansestadt Hamburg
+    </p>
+
+    <h2>Verantwortlich für den Inhalt nach § 55 Abs. 2 RStV</h2>
+    <p>
+      Prof. Dr. Inga Schmidt<br />
+      (Präsidentin der HSBA)<br />
+      Alter Wall 38, 20457 Hamburg
+    </p>
+
+    <h2>Haftungsausschluss</h2>
+    <p>
+      Trotz sorgfältiger inhaltlicher Kontrolle übernehmen wir keine Haftung für die Inhalte externer Links. Für den Inhalt der verlinkten Seiten sind ausschließlich deren Betreiber verantwortlich.
+    </p>
+  </div>
 </body>
 
 <?php include '../includes/footer.php'; ?>
